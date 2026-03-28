00ab2b67ae2f2a469ce9ac759f313c578ec2e97e
Francisco <github.com.visa669@passinbox.com>
2026-03-28 21:44:34 +0100

    Tiktok logo

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
     rolldownOptions: {
       input: {
         index: 'index.html',
-        'england-flag': './src/art/01-england-flag/index.html'
+        'england-flag': './src/art/01-england-flag/index.html',
+        'laughing-emoji': './src/art/02-laughing-emoji/index.html',
+        'pepsi-logo': './src/art/03-pepsi-logo/index.html',
+        'bahamas-flag': './src/art/04-bahamas-flag/index.html',
+        'tiktok-logo': './src/art/05-tiktok-logo/index.html',
       },
     },
   },
