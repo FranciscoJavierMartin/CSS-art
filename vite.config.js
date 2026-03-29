0b43ee38ea6b7296cf34015109d8ca4d47fbb7f6
Francisco <github.com.visa669@passinbox.com>
2026-03-29 14:00:21 +0200

    Duck

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/CSS-art/',
+  base: "/CSS-art/",
   build: {
     rolldownOptions: {
       input: {
-        index: 'index.html',
-        'england-flag': './src/art/01-england-flag/index.html',
-        'laughing-emoji': './src/art/02-laughing-emoji/index.html',
-        'pepsi-logo': './src/art/03-pepsi-logo/index.html',
-        'bahamas-flag': './src/art/04-bahamas-flag/index.html',
-        'tiktok-logo': './src/art/05-tiktok-logo/index.html',
+        index: "index.html",
+        "england-flag": "./src/art/01-england-flag/index.html",
+        "laughing-emoji": "./src/art/02-laughing-emoji/index.html",
+        "pepsi-logo": "./src/art/03-pepsi-logo/index.html",
+        "bahamas-flag": "./src/art/04-bahamas-flag/index.html",
+        "tiktok-logo": "./src/art/05-tiktok-logo/index.html",
+        "duck-art": "./src/art/06-duck-art/index.html",
       },
     },
   },
